305e30af866d1082f6bb40d2ab5564fa36a7f0c8
Alan Ihre <alan.ihre@alanihre.se>
2016-09-20 20:55:33 +0200

    movePoint method in game can be called with both Point and String arguments indicating the position of the piece being moved.

--- src/main/java/com/alanihre/chess/game/Game.java
+++ src/main/java/com/alanihre/chess/game/Game.java
@@ -46,33 +46,44 @@ public abstract class Game {
     public void movePiece(String oldPosition, String newPosition) {
         Point oldPositionPoint = getBoard().labeledPointToBoardPoint(oldPosition);
         Point newPositionPoint = getBoard().labeledPointToBoardPoint(newPosition);
+        movePiece(oldPositionPoint, newPositionPoint);
+    }
 
+    public void movePiece(Point oldPosition, Point newPosition) {
         //Check if any movement was made at all
-        if (oldPositionPoint.equals(newPositionPoint)) {
+        if (oldPosition.equals(newPosition)) {
             throw new InvalidMoveException("Old and new position are equal");
         }
 
-        Piece piece = getBoard().getPieceAtPosition(oldPositionPoint);
+        Piece piece = getBoard().getPieceAtPosition(oldPosition);
         if (piece == null) {
             throw new InvalidMoveException("There is no piece at the entered position");
         }
 
-        if (!canMovePiece(piece, newPositionPoint)) {
+        if (!getBoard().positionWithinBoardBounds(newPosition)) {
+            throw new InvalidMoveException("New position is outside board bounds");
+        }
+
+        if (piece.getColor() != getCurrentMovingPieceColor()) {
+            throw new InvalidMoveException("The piece at the entered position can't be moved by you. It has wrong color.");
+        }
+
+        if (!canMovePiece(piece, newPosition)) {
             //This exception should never be thrown since canMovePiece() should throw exceptions movement errors.
             throw new InvalidMoveException("The move is not allowed");
         }
 
         //Check if piece at target position can be captured
-        Piece targetPositionPiece = getBoard().getPieceAtPosition(newPositionPoint);
+        Piece targetPositionPiece = getBoard().getPieceAtPosition(newPosition);
         if (targetPositionPiece != null && pieceCanCapturePiece(piece, targetPositionPiece)) {
             capturePiece(targetPositionPiece);
         }
 
         getBoard().removePiece(piece);
-        piece.moveTo(newPositionPoint);
+        piece.moveTo(newPosition);
         getBoard().putPiece(piece);
 
-        pieceMoved(piece, oldPositionPoint);
+        pieceMoved(piece, oldPosition);
 
         String readableCoordinate = getBoard().boardPointToLabeledPoint(piece.getPosition());
         getDelegate().pieceMoved(piece, readableCoordinate);
@@ -86,14 +97,6 @@ public abstract class Game {
     abstract void prepareForNextMove();
 
     protected boolean canMovePiece(Piece piece, Point newPosition) {
-        if (!getBoard().positionWithinBoardBounds(newPosition)) {
-            throw new InvalidMoveException("New position is outside board bounds");
-        }
-
-        if (piece.getColor() != getCurrentMovingPieceColor()) {
-            throw new InvalidMoveException("The piece at the entered position can't be moved by you. It has wrong color.");
-        }
-
         if (!piece.canMoveTo(newPosition)) {
             throw new InvalidMoveException("This piece is not allowed to move to the entered position");
         }
