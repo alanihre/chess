c4eab20e9b7e15663ab5bd9b1435c33ac03ded05
Alan Ihre <alan.ihre@alanihre.se>
2016-09-20 21:43:34 +0200

    Added a game delegate to GameTest since it is required for construction of the game class. Piece used in GameTest is a knight instead of a pawn since pawns have some specific movement rules.

--- src/test/java/com/alanihre/chess/game/GameTest.java
+++ src/test/java/com/alanihre/chess/game/GameTest.java
@@ -2,8 +2,9 @@ package com.alanihre.chess.game;
 
 import com.alanihre.chess.Point;
 import com.alanihre.chess.board.Board;
-import com.alanihre.chess.piece.Pawn;
+import com.alanihre.chess.piece.Knight;
 import com.alanihre.chess.piece.Piece;
+import com.alanihre.chess.piece.PieceType;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -19,16 +20,39 @@ public class GameTest {
     private static int PIECE_START_POSITION_Y = 2;
     private Game game;
     private Piece piece;
+    private GameDelegate gameDelegate;
 
     @Before
     public void setUp() {
-        game = new Game(null) {
+        gameDelegate = new GameDelegate() {
+            public void gameReady(Game game) {
+
+            }
+
+            public void requestMove(Piece.PieceColor color) {
+
+            }
+
+            public void pieceCaptured(Piece piece, String coordinate) {
+
+            }
+
+            public void pieceMoved(Piece piece, String coordinate) {
+
+            }
+
+            public PieceType requestNewPieceOfType(PieceType[] availableTypes, String message) {
+                return null;
+            }
+        };
+
+        game = new Game(gameDelegate) {
             @Override
             protected Board initializeBoard() {
                 Board board = new Board(BOARD_SIZE_WIDTH, BOARD_SIZE_HEIGHT) {
                     @Override
                     public void setupBoard() {
-                        piece = new Pawn(new Point(PIECE_START_POSITION_X, PIECE_START_POSITION_Y), PIECE_COLOR);
+                        piece = new Knight(new Point(PIECE_START_POSITION_X, PIECE_START_POSITION_Y), PIECE_COLOR);
                         this.putPiece(piece);
                     }
                 };
